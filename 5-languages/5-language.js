d8d7d1aeb9aef2948458be955d216efda5725d2d
Inna <sis.nlled@gmail.com>
2026-01-06 21:39:40 +0400

    Второе задание

---
+++ 5-languages/5-language.js
@@ -0,0 +1,17 @@
+const language = 'de'
+
+switch(language) {
+ case 'en':
+    console.log("Good afternoon!");
+    break;
+ case 'de':
+    console.log("Gutten tag!");
+    break;
+ case 'ru':
+    console.log("Добрый день!");
+    break;
+default:
+    console.log("Язык не распознан");
+    break;
+}
+
