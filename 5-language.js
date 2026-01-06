484653b4bf57d5a22093b2d8e8c834e3ff404c62
Inna <sis.nlled@gmail.com>
2026-01-06 21:10:17 +0400

    2-d hw 06/01/26

---
+++ 5-language.js
@@ -0,0 +1,16 @@
+const language = 'de'
+
+switch(language) {
+ case 'en':
+    console.log("Hello");
+    break;
+ case 'de':
+    console.log("Hallo");
+    break;
+ case 'es':
+    console.log("Hola");
+    break;
+default:
+    console.log("Язык не распознан");
+    break;
+}
